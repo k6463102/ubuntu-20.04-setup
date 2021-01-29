09a8e11ba26068fd3dafb1cf0afca8aa020c5eab
Ikeda Ryo <k6463102@kadai.jp>
2021-01-29 12:26:48 +0900

    add: インストール自動化スクリプトを追加

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install build-essential vim tmux manpages-ja manpages-ja-dev -y
+
+sudo apt-get install gdebi -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
+
+sudo apt-get install htop tree nkf curl filezilla gnome-tweaks -y
