74ffd03e54d8e8075045636219f037dd633fe5fb
Ikeda Ryo <k6463102@kadai.jp>
2021-01-29 12:48:46 +0900

    fix: gitインストールと不要パッケージ削除の処理を追加

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ set -x
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install build-essential vim tmux manpages-ja manpages-ja-dev -y
+sudo apt-get install build-essential vim tmux git manpages-ja manpages-ja-dev -y
 
 sudo apt-get install gdebi -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi google-chrome-stable_current_amd64.deb
 
 sudo apt-get install htop tree nkf curl filezilla gnome-tweaks -y
+
+sudo apt-get autoremove -y
