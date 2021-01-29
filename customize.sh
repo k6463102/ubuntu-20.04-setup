bc2d2a7a3da0101c638d526fe6a9c0e00c6bbc11
Ikeda Ryo <k6463102@kadai.jp>
2021-01-29 13:22:37 +0900

    remove: ドックにゴミ箱を表示する設定を削除

--- customize.sh
+++ customize.sh
@@ -10,4 +10,3 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans CJK JP 11'
 
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
